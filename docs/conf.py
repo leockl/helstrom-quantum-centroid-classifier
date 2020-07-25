0df6e32bc4bcd0addd5632a263f0bd215b71f3dc
Leo Chow <leo.chow11@gmail.com>
2020-07-26 02:02:51 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ author = 'Leo Chow, Giuseppe Sergioli, Roberto Giuntini.'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
