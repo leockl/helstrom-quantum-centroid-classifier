40894e852e2b07158bfe51ea894c5c0ab5fe2bf6
Leo Chow <leo.chow11@gmail.com>
2020-07-26 02:33:35 +1000

    Update test_template.py

--- hqc/tests/test_template.py
+++ hqc/tests/test_template.py
@@ -5,7 +5,7 @@ from sklearn.datasets import load_iris
 from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.testing import assert_allclose
 
-from hqc import hqc
+from hqc import HQC
 
 
 @pytest.fixture
@@ -14,9 +14,13 @@ def data():
 
 def test_template_classifier(data):
     X, y = data
-    clf = hqc()
+    clf = HQC()
     assert clf.rescale == 1
+    assert clf.encoding == 'amplit'
     assert clf.n_copies == 1
+    assert clf.class_wgt == 'equi'
+    assert clf.n_jobs == 1
+    assert clf.n_splits == 1
 
     clf.fit(X, y)
     assert hasattr(clf, 'classes_')
