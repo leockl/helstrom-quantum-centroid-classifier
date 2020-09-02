8cdfefd5e16cfdb75082d3a529a9976cafba93de
Leo Chow <leo.chow11@gmail.com>
2020-09-02 12:11:49 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['numpy', 'scipy', 'scikit-learn', 'joblib']
+    install_requires=['numpy', 'scipy', 'scikit-learn', 'joblib', 'sphinx', 'sphinx-gallery', 'sphinx_rtd_theme']
 )
 
